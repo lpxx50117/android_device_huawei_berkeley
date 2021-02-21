13160b0edd4e3e2caba6ec0085d3f4b1cca54eb8
lpxx50117 <54358415+lpxx50117@users.noreply.github.com>
2021-02-21 10:32:27 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,10 +28,10 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x8000 --ramdisk_offset 0x01000000 --tag
 
 # phony empty kernel to satisfy build system, but this device does not
 # include a kernel in the recovery image -- flash to recovery_ramdisk
-TARGET_PREBUILT_KERNEL := device/huawei/berkeley/dummykernel
+TARGET_PREBUILT_KERNEL := device/huawei/cornell/dummykernel
 # else uncomment below to build from sauce
-# TARGET_KERNEL_SOURCE := kernel/huawei/berkeley
-# TARGET_KERNEL_CONFIG := berkeley_defconfig
+# TARGET_KERNEL_SOURCE := kernel/huawei/cornell
+# TARGET_KERNEL_CONFIG := cornell_defconfig
 
 # NOTE I only bother properly setting recovery image size
 BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
@@ -60,3 +60,5 @@ TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
 BOARD_CUSTOM_BOOTIMG_MK := device/huawei/berkeley/custombootimg.mk
 # MTP will not work until we update it to support ffs
 TW_EXCLUDE_MTP := true
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := zh_CN
